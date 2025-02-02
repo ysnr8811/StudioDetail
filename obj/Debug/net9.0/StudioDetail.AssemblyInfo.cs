27cadc5dacc0b1c536f7c2757301a31e36c8484c
ysnr8811 <86781060+ysnr8811@users.noreply.github.com>
2025-02-02 23:46:18 +0900

    assembly

--- obj/Debug/net9.0/StudioDetail.AssemblyInfo.cs
+++ obj/Debug/net9.0/StudioDetail.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudioDetail")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0af06ce40e0addf2890988aaf537cff4ec98dfe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d240855fec732a2a81d15acd63f91e35c24c72a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudioDetail")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudioDetail")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
