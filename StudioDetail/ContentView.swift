aed0d0b2b616105409b6ed6abcf1327a1f6e8455
ysnr8811 <86781060+ysnr8811@users.noreply.github.com>
2025-03-25 19:09:50 +0900

    サイドのビュー部分の実装を別ファイルへ移動

--- StudioDetail/ContentView.swift
+++ StudioDetail/ContentView.swift
@@ -74,38 +74,7 @@ struct ContentView: View {
 }
 
 // ドロワービュー
-struct Drawer: View {
-    @Binding var selection: String? // 選択されたナビゲーション項目
-    @Binding var isDrawerOpen: Bool // ドロワーの開閉状態
 
-    var body: some View {
-        VStack(alignment: .leading) {
-            Group {
-                // 各ナビゲーションリンク
-                NavigationLink(value: "HOME") {
-                    Text("HOME")
-                }
-                NavigationLink(value: "ABOUT US") {
-                    Text("ABOUT US")
-                }
-                NavigationLink(value: "WORK") {
-                    Text("WORK")
-                }
-                NavigationLink(value: "CONTACT") {
-                    Text("CONTACT")
-                }
-                NavigationLink(value: "PRIVACY POLICY") {
-                    Text("PRIVACY POLICY")
-                }
-            }
-            .padding() // 各リンクにパディングを追加
-            Spacer() // 下部にスペースを追加
-        }
-        .frame(width: 200) // ドロワーの幅を設定
-        .background(Color(.systemGray6)) // 背景色を設定
-        .edgesIgnoringSafeArea(.bottom) // 下部の安全領域を無視
-    }
-}
 
 // 各ビューの定義
 struct HomeView: View {
